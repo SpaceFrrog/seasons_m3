5813a361302d201a1449bb999e8416608bf8fe06
SpaceFrrog <105725665+SpaceFrrog@users.noreply.github.com>
2026-04-23 13:38:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,4 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installiert');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Hier könnte später das Caching für den Offline-Modus stehen
+self.addEventListener('fetch', function(event) {
+  // Tut gar nichts, leitet nur weiter
+  event.respondWith(fetch(event.request));
 });
